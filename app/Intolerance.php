972de7700ff9da8374e873618aa974ac2a26cd31
Giovanni Porcelli Minervini <giovannipor23@gmail.com>
2021-06-11 12:26:34 +0200

    intolerances seeder

---
+++ app/Intolerance.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Intolerance extends Model
+{
+    protected $fillable = [
+        'name'
+    ];
+}
---
+++ database/seeds/IntolerancesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Intolerance;
+
+class IntolerancesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $intolerances = config('intolerances');
+        foreach($intolerances as $intolerance) {
+            $new_intolerance = new Intolerance();
+            $new_intolerance->name = $intolerance['name'];
+            $new_intolerance->save();
+        }
+    }
+}
