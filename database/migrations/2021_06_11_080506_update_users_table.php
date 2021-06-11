54553fb7c1eca8823585c7368af8a5a92ad9861c
andrealigorio <andrea85kro@gmail.com>
2021-06-11 10:06:06 +0200

    users migartion

--- database/migrations/2021_06_11_080506_update_users_table.php
+++ database/migrations/2021_06_11_080506_update_users_table.php
@@ -14,7 +14,10 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('reference_name');
+            $table->string('address');
+            $table->string('vat_number', 11)->unique();
+            $table->float('rating', 2, 1)->nullable();
         });
     }
 
@@ -26,7 +29,10 @@ class UpdateUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('reference_name');
+            $table->dropColumn('address');
+            $table->dropColumn('vat_number');
+            $table->dropColumn('rating');
         });
     }
 }
