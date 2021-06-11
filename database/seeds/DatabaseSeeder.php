744c4f125c259458f2d058fc4ed880d6fd2d37f1
federico <federico1993.d.alessio.1993@gmail.com>
2021-06-11 16:39:58 +0200

    aggiornamento DatabaseSedder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(TypologyTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(IntolerancesTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
     }
 }
