44a2832e3759783719593e213e8fd4c836129e34
federico <federico1993.d.alessio.1993@gmail.com>
2021-06-15 09:07:30 +0200

    details

--- resources/views/guest/restourant.blade.php
+++ resources/views/guest/restourant.blade.php
@@ -2,7 +2,7 @@
 <ul>
     @foreach ($restourants as $restourant)
     <li>
-        {{$restourant->name}}  <a href="{{ route('products_index', $restourant->id) }}"><input type="button"value="SHOW" ></a>
+        {{$restourant->name}}  <a href="{{ route('products_index', $restourant->id) }}"><input type="button"value="VEDI MENU'" ></a>
     </li>
     @endforeach
 </ul>
